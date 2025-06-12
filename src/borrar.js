6a89cea2aa8ee1dcbf9ea7801b89fe5ff71c0973
Maria Fernanda Sosa <mariafernandasosa02@hotmail.com>
2025-06-12 04:30:01 -0300

    Detalles

--- src/borrar.js
+++ src/borrar.js
@@ -1,11 +1,9 @@
 export function borrar() {
+    
+    document.getElementById('op1').value = '';
+    document.getElementById('op2').value = '';
+    document.getElementById('resultado').textContent = '';
+    document.getElementById('operador').value = '+';
 
-    let op1 = parseFloat(document.getElementById('op1').value);
-    let op2 = parseFloat(document.getElementById('op2').value);
-    let operador = document.getElementById('operador').value;
-    const elemResultado = document.getElementById('resultado');
- 
-    op1 = '';
-    op2 = '';
-    elemResultado.style.display = "none"
+    document.getElementById('resultado').style.display = "none"
 }
